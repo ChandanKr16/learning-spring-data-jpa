03bb0352917bc89357caf9e218ae3696656c165d
ChandanKr16 <chandan.kumar2021@vitstudent.ac.in>
2023-02-06 22:26:24 +0530

    Empty project created:

---
+++ src/test/java/me/chandankumar/learningspringdatajpa/LearningSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chandankumar.learningspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
