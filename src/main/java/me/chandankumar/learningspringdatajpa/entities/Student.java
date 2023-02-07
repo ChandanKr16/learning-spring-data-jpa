770b6444f0f55d40d28f3d40cdce211bd0384963
ChandanKr16 <chandan.kumar2021@vitstudent.ac.in>
2023-02-07 12:32:19 +0530

    Created Student JPA Entity

---
+++ src/main/java/me/chandankumar/learningspringdatajpa/entities/Student.java
@@ -0,0 +1,34 @@
+package me.chandankumar.learningspringdatajpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Student")
+@Table(name = "student",
+        uniqueConstraints = {
+        @UniqueConstraint(name = "student_email_unique", columnNames = "email")
+})
+@Data
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @SequenceGenerator(name = "student_seq", sequenceName = "student_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_seq")
+    @Column(updatable = false)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String firstName;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Integer age;
+
+}
