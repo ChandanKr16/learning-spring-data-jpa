97d7dee9ac37832157ce73a0ec656d1910bf660b
ChandanKr16 <chandan.kumar2021@vitstudent.ac.in>
2023-02-14 14:33:07 +0530

    Created EnrollmentId embedable and Enrollment entity

---
+++ src/main/java/me/chandankumar/learningspringdatajpa/entities/Enrollment.java
@@ -0,0 +1,32 @@
+package me.chandankumar.learningspringdatajpa.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "Enrollment")
+@Table(name = "enrollment")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Enrollment {
+
+    @EmbeddedId
+    private EnrollmentId id;
+
+    @ManyToOne
+    @MapsId("studentId")
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+
+    @ManyToOne
+    @MapsId("courseId")
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+}
---
+++ src/main/java/me/chandankumar/learningspringdatajpa/entities/EnrollmentId.java
@@ -0,0 +1,24 @@
+package me.chandankumar.learningspringdatajpa.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class EnrollmentId implements Serializable {
+
+    @Column(name = "student_id")
+    private Long studentId;
+
+    @Column(name = "course_id")
+    private Long courseId;
+
+
+}
